cc853bdc5e01f3f681bc147763e0ba1564e73e20
sattwik742 <sattwik211@gmail.com>
2025-09-29 21:41:17 +0530

    Fifth Commit

--- src/data/education.js
+++ src/data/education.js
@@ -1,7 +1,22 @@
-const education=
-[
-    {
-        id: 1,
-        school: " "
-    }
-]
+const education =
+    [
+        {
+            id: 1,
+            school: "Kalinga Institute of Industrial Technology",
+            degree: "Master of Computer Application",
+            period: "2025-2027",
+        },
+        {
+            id: 2,
+            school: "RCC Institute of Information Technology",
+            degree: "Bachelor of Computer Application",
+            period: "2022-2025",
+        },
+        {
+            id: 3,
+            school: "Baranagar Narendranath Vidyamandir",
+            degree: "Higher Secondary (Class XII)",
+            period: "2019-2021"
+        }
+    ];
+export default education;
